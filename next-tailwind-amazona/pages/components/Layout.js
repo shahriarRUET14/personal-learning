fd9fa52dc6191f391160c7481ed3bb30a38d8d92
shahriarRUET14 <shahriar.Ruet14@gmail.com>
2023-02-13 00:18:08 +0600

    Upto Lecture 7

---
+++ next-tailwind-amazona/pages/components/Layout.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + "-Amazona" : "Amazona"}</title>
+        <meta name="description" content="Ecom Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4 px">
+            <Link className="text-lg font-bold" href="/">
+              Amazona
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          @Copyright @Shahriar
+        </footer>
+      </div>
+    </>
+  );
+}
